d93f853852284eba99dd7e1459f1eae3b9cf0c7c
Jordan McBride <jim3@email.uscb.edu>
2026-02-18 23:48:16 -0500

    added full email for author

--- src/VariableLengthArgApp.java
+++ src/VariableLengthArgApp.java
@@ -4,9 +4,28 @@
  */
 
 /**
- *
- * @author jordan
+ *This calculates the product of a series of integers that is passed to a method 
+ * called product using a variable length argument list 
+ * @author jim3@email.uscb.edu
  */
 public class VariableLengthArgApp {
-    
+ 
+/*INTIALIZATION PHASE */
+//Decalre a method that accpets a varibale number of ints
+//Declare a variable whiches stores the product
+// Set the project as 1
+
+/*PROCESSING PHASE */
+// Set up a loop to go thorugh each interger that is passed to the method
+//Multiply the product by each int
+
+/* TERMINATION PHASE */
+// Return the final product value 
+
+
+
+
+
+
+
 }
